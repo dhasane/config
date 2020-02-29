ef7192d344f7a47a8a7e8d76e4a813a163480dc2
dhasane <danihas@live.com>
2020-02-28 22:08:49 -0500

    ya pensé en una forma de poder hacerle seguimiento a carpetas completas a pesar de que no se muestren todos los archivos modificados, ahora hay el comando config-add para esto, aunque creo que aun falta arreglar unos detalles

--- .config/zsh/themes/tt.zsh
+++ .config/zsh/themes/tt.zsh
@@ -4,89 +4,6 @@
 # 	re5et
 # 	simonoff
 
-# vim_ins_mode="%{$fg[cyan]%}[INS]%{$reset_color%}"
-# vim_cmd_mode="%{$fg[green]%}[CMD]%{$reset_color%}"
-# vim_mode=$vim_ins_mode
-#
-# function zle-keymap-select {
-#   vim_mode="${${KEYMAP/vicmd/${vim_cmd_mode}}/(main|viins)/${vim_ins_mode}}"
-#   zle reset-prompt
-# }
-# zle -N zle-keymap-select
-#
-# function zle-line-finish {
-#   vim_mode=$vim_ins_mode
-# }
-# zle -N zle-line-finish
-# # Fix a bug when you C-c in CMD mode and you'd be prompted with CMD mode indicator, while in fact you would be in INS mode
-# # Fixed by catching SIGINT (C-c), set vim_mode to INS and then repropagate the SIGINT, so if anything else depends on it, we will not break it
-# # Thanks Ron! (see comments)
-# function TRAPINT() {
-#   vim_mode=$vim_ins_mode
-#   return $(( 128 + $1 ))
-# }
-
-
-# if [ "$USER" = "root" ]; then CARETCOLOR="red"; else CARETCOLOR="magenta"; fi
-
-# Prompt
-#
-# Below are the color init strings for the basic file types. A color init
-# string consists of one or more of the following numeric codes:
-# Attribute codes:
-# 00=none 01=bold 04=underscore 05=blink 07=reverse 08=concealed
-# Text color codes:
-# 30=black 31=red 32=green 33=yellow 34=blue 35=magenta 36=cyan 37=white
-# Background color codes:
-# 40=black 41=red 42=green 43=yellow 44=blue 45=magenta 46=cyan 47=white
-
-# setprompt () {
-
-	###
-    # Modify Git prompt
-    # ZSH_THEME_GIT_PROMPT_PREFIX=" ["
-    # ZSH_THEME_GIT_PROMPT_SUFFIX="]"
-#
-# local      user='%{$fg_bold[cyan]%}%n%{$reset_color%}'
-# local        at='%{$fg[yellow]%}@%{$reset_color%}'
-# local      host='%{$fg_bold[blue]%}%m%{$reset_color%}'
-# local  conector='%{$fg_bold[yellow]%}::%{$reset_color%}'
-# local ubicacion='%{${fg_bold[green]}%}%~%{$reset_color%}'
-# local       git='${(e)PR_FILLBAR}$PR_CYAN$(git_prompt_info)%{$reset_color%}'
-#
-#
-# PROMPT="
-# ${user}${at}${host}${conector}$ubicacion${git}
-# %{${fg[$CARETCOLOR]}%}> %{${reset_color}%}"
-#
-# }
-#
-# setprompt
-#
-# local return_code="%(?..%{$fg_bold[red]%}:( %?%{$reset_color%})"
-#
-# RPROMPT='${return_code} ${vim_mode}'
-#
-#
-#
-
-
-
-
-
-
-
-
-
-
-
-# awesome theme :D
-# utilizando como referencia:
-# 	re5et
-# 	simonoff
-
-# me gustaria pensar una mejor manera de mostrar el modo de uso
-
 # vim_ins_mode="%{$fg_bold[cyan]%}[INS]%{$reset_color%}"
 vim_cmd_mode="%{$fg_bold[green]%}[CMD]%{$reset_color%}"
 vim_mode=$vim_ins_mode
@@ -151,7 +68,7 @@ function precmd {
     local total="$(($prompt+$addsize))"
 
     if [[ "$total" -gt $TERMWIDTH ]]; then
-        PR_PWDLEN="$(($TERMWIDTH - $total))"
+        ((PR_PWDLEN=$TERMWIDTH - "$total" ))
     else
         PR_FILLBAR="\${(l.(($TERMWIDTH - ($total)))..${PR_SPACE}.)}"
     fi
@@ -179,7 +96,7 @@ setprompt () {
 
 
 ###
-# The prompt.
+# Finally, the prompt.
 
     local      user='%{$fg_bold[cyan]%}%n%{$reset_color%}'          # nombre de usuario
     local        at='%{$fg[yellow]%}@%{$reset_color%}'              # simbolo @
---
+++ .vimrc
@@ -0,0 +1,5 @@
+" source ~/.config/nvim/init.vim
+set nocompatible
+filetype plugin on
+
+set nrformats=
